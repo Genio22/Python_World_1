a653258f790d9580f85fc966b094f341613cfd61
Genio22 <104478212+Genio22@users.noreply.github.com>
2022-06-13 00:35:01 +0600

    first commit

--- Practice 5(Random guss game).py
+++ Practice 5(Random guss game).py
@@ -1,16 +1,21 @@
-from ntpath import join
+#from ntpath import join
 import random
 
-random_numbers = random.randint(0, 10)
-secret_number = '>> % s'%(random_numbers)
+random_numbers = random.randint(0, 9)
+
 guess_number =  0
 guess_limit = 3
 while guess_number < guess_limit:
-    guess = int(input(f'Guess a number between 1 to 10 >> '))
+    int_convert = ' % s'%(random_numbers)
+    secret_number = int(int_convert)
+    guess = int(input(f'Guess a number between 1 to 10  >> '))
     guess_number += 1
+#    print (guess == secret_number)
     if guess == secret_number:
+        
         print('You win!')
-    else:
-        print(f'You Fail {secret_number}')
         break
+else:
+    print(f'You Fail {secret_number}')
+#    break
 
