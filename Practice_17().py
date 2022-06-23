58b4cc1177fc3aabc0b13059517d1cee8fab70f1
Genio22 <104478212+Genio22@users.noreply.github.com>
2022-06-24 00:25:33 +0600

    first commit

--- Practice_17().py
+++ Practice_17().py
@@ -1,2 +1,14 @@
-f = 'Windows ©️👀'
-print(f)
+def emoji_convert(message):
+    words = message.split(" ")
+    emoji = {
+        ":)" : "😃",
+        ":(" : "😞"
+    }
+    output = ""
+    for word in words:
+        output += emoji.get(word, word) + " "
+    return output
+
+
+message = input('>')
+print(emoji_convert(message))
